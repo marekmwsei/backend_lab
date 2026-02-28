589a9730f3d73a5248afcf95c9fb6007141c6d78
marek.machula <marek.machula@microsoft.wsei.edu.pl>
2026-02-28 11:01:10 +0100

    init

--- CoreApp/Repositories/GenericRepository.cs
+++ CoreApp/Repositories/GenericRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace CoreApp.Repositories;
 
-public class GenericRepository
+public interface IGenericRepository
 {
+    Task<T?> AddAsync(T entity);
+    Task<T?> FindByIdAsync(int id);
+    Task<IEnumerable<T>> FindAllAsync();
+    Task DeleteByIdAsync (int id);
     
 }
