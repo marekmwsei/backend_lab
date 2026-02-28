1a4a9d867aba3e916961f238c5c8e6ddfa3074f1
marek.machula <marek.machula@microsoft.wsei.edu.pl>
2026-02-28 11:08:04 +0100

    poprawka

---
+++ Infrastructure/MemoryStudentRepository.cs
@@ -0,0 +1,29 @@
+﻿using CoreApp.Models;
+using CoreApp.Infrastructure;
+
+namespace Infrastructure;
+
+public class MemoryStudentRepository
+{
+    public Task<Student?> AddAsync(Student entity)
+    {
+        throw new NotImplementedException();
+    } 
+    
+    public Task<Student?> FindByIdAsync(int id)
+    {
+        return new Student()
+        {
+            Id = 1,
+            FirstName = "Adam",
+            LastName = "Nowak",
+            BirthDate = new DateOnly(2000, 1, 1),
+            Email = "adam@wsei.edu.pl",
+            StudyYearFields = new List<StudyYearField>()
+            {
+                StudyField = new StudyYearField() { Id = 100, Title = "Informatyka" },
+                StudyYear = new StudyYear() { Id = 1000, Name = "2025/2026 Niestacjonarne", AcademicYear = DateOnly.Parse("2025-09-01") }
+            }
+        };
+    }
+}
